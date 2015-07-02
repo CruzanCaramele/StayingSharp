761c00f8c08ce792081aab68272296320325ea9c
CruzanCaramele <letters2hamza@gmail.com>
2015-07-02 19:14:31 +0800

    replace string name and plus signs with {} and using .format function

---
+++ lumberjack.py
@@ -0,0 +1,7 @@
+name = raw_input("what is your name ? ")
+#condition
+if name == "Kenneth":
+	print( "{} is a lumberjack, he is ok!".format(name))
+else:
+	print("{} Sleeps all nitght  {}"  
+		" works all day".format(name, name))
