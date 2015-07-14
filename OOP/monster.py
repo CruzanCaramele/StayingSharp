88ffdeb02370547a083278ee1072cdd9800d5d25
CruzanCaramele <letters2hamza@gmail.com>
2015-07-14 22:11:38 +0800

    Replace the attributes in __init__ function with **kwargs for dictionary unpacking in order to habdle what ever attributes is passed from the object of the monster class

--- OOP/monster.py
+++ OOP/monster.py
@@ -1,12 +1,20 @@
-class Monster(object):
 
-	def __init__(self,hit_points, weapon, color, sound):
-		self.hit_points = hit_points 
-		self.weapon = weapon 
-		self.color = color 
-		self.sound = sound 
+class Monster(object):
+	"""This calls initializes a monster with certain
+	    attributes
+	     """
+	def __init__(self,**kwargs):
+		"""All objects in python have the init method.
+		    It sets attributes on creation. It uses a 
+		    dictionary unpacking (**kwargs).
+		 """
+		self.hit_points = kwargs.get("hit_points", 1) 
+		self.weapon = kwargs.get("weapon", "sword") 
+		self.color = kwargs.get("color", "yellow") 
+		self.sound = kwargs.get("sound", "ROAR") 
 
 
 
 	def battlecry(self):
 		return self.sound.upper()
+
