026f7b4aadb727401dd04737fd4809464a589795
CruzanCaramele <letters2hamza@gmail.com>
2015-07-22 16:47:50 +0800

    feat:Add Context dictionary to routes Make use of context as a dictionary containing a pair of keys with their values as the variables within each route to be passed to the corresponding rendered templates

--- Character/simple_app.py
+++ Character/simple_app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 #from flask import request
 
 #use the current name space
@@ -10,14 +11,16 @@ app = Flask(__name__)
 def  index(name="Iceland"):
 	#get argument from the browser url
 	#name = request.args.get("name", name)
-	return "Hello from here {}".format(name)
+	context = {"name": name}
+	return render_template("index.html", **context)
 
 
 @app.route("/add/<int:num1>/<int:num2>")
 @app.route("/add/<float:num1>/<float:num2>")
 def add(num1, num2):
 	#return str(num1 + num2)
-	return "{} + {} = {}".format(num1, num2,  num1 + num2)
+	context = {"num1": num1, "num2": num2}
+	return render_template("add.html",**context)
 
 
 app.run(debug=True, port=8000, host="0.0.0.0")
