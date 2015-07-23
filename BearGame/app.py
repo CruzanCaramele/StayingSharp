c04ca32bb0fc33f19b1baa539bfe2c106d542f02
CruzanCaramele <letters2hamza@gmail.com>
2015-07-23 17:03:49 +0800

    feat: Add view(route) for bear builder called builder This view renders the builter template containing out bear. The saves paramenter with its value as the cookie dictionary is passwd to the template as well as the imported Defualts from the options file.

--- BearGame/app.py
+++ BearGame/app.py
@@ -1,6 +1,7 @@
 import json
 from flask import (Flask, render_template, redirect, 
 		     url_for, request, make_response)
+from options import DEFAULTS
 
 app = Flask(__name__)
 
@@ -26,10 +27,18 @@ def save():
 	#import pdb; pdb.set_trace()
 	#cookies are set on the respons, things that go back to the
 	#browser
-	response = make_response(redirect(url_for("index")))
+	response = make_response(redirect(url_for("builder")))
 	data = get_saved_data()
 	data.update(dict(request.form.items()))
 	response.set_cookie("character", json.dumps(data))
 	return response
+
+
+@app.route("/builder")
+def builder():
+	context = {"saves": get_saved_data(),
+		      "options": DEFAULTS
+		    }
+	return render_template("builder.html", **context)
 
 app.run(debug=True, host="0.0.0.0", port=8020)
