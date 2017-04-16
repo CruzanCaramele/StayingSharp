0cd9e6cf24a2f684c9a8a1cec5bf1b23c016338e
CruzanCaramele <letters2hamza@gmail.com>
2017-04-16 13:09:23 +0800

    hello, i am gopher

---
+++ Go/src/hello/main.go
@@ -0,0 +1,10 @@
+// all go programs must have a main package and one main function
+package main
+
+// package imports go after the package definition
+import "fmt"
+
+// entry point for the program
+func main() {
+	fmt.Println("Hello World")
+}
