adfea90b297e75bd160fd091d80fc40f8a339222
CruzanCaramele <letters2hamza@gmail.com>
2017-05-09 23:32:04 +0800

    determine hiest ranking number

---
+++ Go/projects/src/conditionals/if1.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func main() {
+	firstRank  := 39
+	secondRank := 614
+
+	if firstRank < secondRank {
+		fmt.Println("\nFirst course is doing better" +
+			" than second course")
+	} else if firstRank > secondRank {
+		fmt.Println("\nOh dear... your first" +
+			"course must be doing abysmally!")
+	} else {
+		fmt.Println("\nBoth courses are either" +
+			"the same or something is weir is going on")
+	}
+}
